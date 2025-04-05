54888aa93c5e4e7df67bcf21f605b1a12f4e27f1
Aman Saini <mr.amannobody@gmail.com>
2025-04-06 00:07:44 +0530

    i have changed it to env.!! let's see if it works or not in production

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -3,6 +3,10 @@ import axios from "axios";
 // export const axiosInstance = axios.create({
 //     baseURL: "http://localhost:6060/api",
 // })
+// export const axiosInstance = axios.create({
+//     baseURL: "https://sequencer-gbvb.onrender.com/api",
+// })
+
 export const axiosInstance = axios.create({
-    baseURL: "https://sequencer-gbvb.onrender.com/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 })
